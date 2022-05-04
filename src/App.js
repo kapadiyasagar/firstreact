19325e3006e621b623f22dad92feabfb5db521f3
kapadiyasagar <sagarkapadiya220@gmail.com>
2022-05-04 10:53:45 +0530

    ko

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import matchers from '@testing-library/jest-dom/matchers';
+import Contry from './componet/container/contry';
 
 function App() {
 
-  
+
+  return(
+    <>
+      <Contry/>
+    </>
+  )  
 
 
-  
-
-  
 }
 
 export default App;
