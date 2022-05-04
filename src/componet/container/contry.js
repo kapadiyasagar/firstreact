8b26576bcd655f3f9e52a27265b3c591fb0e3bcd
kapadiyasagar <sagarkapadiya220@gmail.com>
2022-05-04 10:53:34 +0530

    ok

---
+++ src/componet/container/contry.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Contry extends Component {
+
+    constructor(props){
+        super(props);
+    
+    this.state={
+        contry:'india'
+    };
+}
+
+changeContry=()=>{
+    this.setState{{
+        contry:'uk'
+    }};
+}
+ 
+
+
+    render() {
+        return (
+            <>
+            <div>Contry: {this.state.contry}</div>
+            <button onClick={()=> this.changeContry()}change</button>
+            </>
+        );
+    }
+}
+
+export default Contry;
