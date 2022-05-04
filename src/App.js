ef8d70aa7d176635a1e6ca04c4fbf905176c0018
kapadiyasagar <sagarkapadiya220@gmail.com>
2022-05-04 09:46:23 +0530

    ss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import matchers from '@testing-library/jest-dom/matchers';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  // arry-copy
+
+  // let arr =[10,20,30];
+
+  // // copy-arr
+
+  // let arr1=[...arr];
+  // console.log(arr1);
+
+  // // merge-arr
+
+  // let arr2 =[90,...arr,100];
+  // console.log(arr2);
+
+  // // destructure-arr
+
+  // let [eng,guj,ss,sci]=arr;
+  // console.log(ss);
 }
 
+// Object-copy
+
+
+let obj={
+  id:101,
+  name:"sagar"
+};
+
+// copy
+
+let obj1={...obj};
+console.log(obj1);
+
+
+// merge
+
+let obj2={
+  ...obj,
+  place:"surat",
+  name:"ajay"
+};
+
+// destructure
+
+let{id,name}=obj
+console.log(id,name);
+
+console.log(obj2);
 export default App;
