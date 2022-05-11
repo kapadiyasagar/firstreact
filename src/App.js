df5ceef100b393a79f1db69f7f23e245c1deea36
kapadiyasagar <sagarkapadiya220@gmail.com>
2022-05-11 10:47:15 +0530

    class-base-componate cycle

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import Contry from './componet/container/contry';
 import State from './componet/state';
 import Cstate from './componet/container/changecity state';
 import Branch from './componet/container/task';
+import Cycle from './componet/container/componet-cycle-method/cycle';
+import Time from './componet/container/componet-cycle-method/cycle';
 
 function App() {
 
-  console.log("fsdaf")
+  // console.log("fsdaf")
   
   return(
     <>
-  <Branch/>
+  <Time />
   </>
 )
 
