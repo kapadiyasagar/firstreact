906bbda55c70f50e3cc6df397143cc4e2d6ff02e
kapadiyasagar <kapadiyasagar220@gmail.com>
2022-05-11 19:32:24 +0530

    init

--- src/App.js
+++ src/App.js
@@ -2,85 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import matchers from '@testing-library/jest-dom/matchers';
 import Contry from './componet/container/contry';
+import Time from './componet/container/componet-life-cycle/classbase';
+
+
+
 
 function App() {
 
+    return (
+        <div className='App'>
+            <Time />
 
-  const empolay = [
-    {
-        name: "amit",
-        age: 35,
-        salary: 40000,
-        bonus: 1000,
-        status: true
-    },
-    {
-        name: "ajay",
-        age: 25,
-        salary: 38000,
-        bonus: 2000,
-        status: false
-    },
-    {
-        name: "mayur",
-        age: 23,
-        salary: 50000,
-        bonus: 500,
-        status: true
-    },
-    {
-        name: "jay",
-        age: 29,
-        salary: 35000,
-        bonus: 600,
-        status: true
-    },
-    {
-        name: "raj",
-        age: 33,
-        salary: 22000,
-        bonus: 2000,
-        status: true
-    },
-]
 
-let filterdata=empolay.filter((d,i)=>(d.status==true))
-console.log(filterdata)
+        </div>
+    );
 
-let total=filterdata.reduce((acc,d) =>acc +d.salary+d.bonus,0)
-console.log(total);
 
-return (
-  <>
-      <table border="1">
-          <tr>
-              <td>name</td>
-              <td>age</td>
-              <td>salary</td>
-              <td>bonus</td>
-              <td>status</td>
-              <td>salary+bonus</td>
-              <td>total</td>
-          </tr>
-          {
-              empolay.map((d, i) => {
-                  return (
-                      <tr>
-                          <td>{d.name}</td>
-                          <td>{d.age}</td>
-                          <td>{d.salary}</td>
-                          <td>{d.bonus}</td>
-                          <td>{d.status.toString()}</td>
-                          <td>{d.salary+d.bonus}</td>
-                          {i==0?<td rowSpan={filterdata.length}>{total}</td>:null}
-                      </tr>
-                  )
-              }
-              )
-          }
-      </table>
-  </>
-)
 }
 
 
---
+++ src/componet/container/componet-life-cycle/classbase.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+class Time extends Component {
+
+    constructor(props) {
+        super(props);
+
+
+        this.state = {
+
+            time: new Date()     //1constructior set initial value
+        }
+    }
+
+    tick = () => {
+        debugger;
+        this.setState({
+            time: new Date()
+        })
+        console.log("tick");
+    }
+
+
+    //3 use this methos for requst data to server
+
+    componentDidMount = () => {
+        this.timeId = setInterval(() => this.tick(), 1000)
+    }
+
+    // 4 
+    componentDidUpdate = (prevprops, prevstate) => {
+        if (prevstate.time !== this.state.time) {
+            console.log("componentDidUpdate is called")
+        }
+    }
+
+    //5 componet will umout  used to realse resourse from compontent
+    componentWillUnmount = () => {
+        clearInterval(this.timeId);
+    }
+
+
+    render() {
+        // 2 call render method each time when state value change
+        return (
+            <div>
+                {this.state.time.toLocaleTimeString()}
+            </div>
+        );
+    }
+}
+export default Time;
